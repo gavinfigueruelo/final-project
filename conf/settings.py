2f047fb0de166d5db7d697e23bcac358e484236a
gavinfigueruelo <figueruelogavin@gmail.com>
2021-03-15 13:16:33 -0400

    changes

--- conf/settings.py
+++ conf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jubja3^r8zj++ec_7ut@3)@&6k_3h0baxq3idq1#b4+shx&ox1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+#changes changes
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
