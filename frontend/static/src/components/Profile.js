70d8e02c907178d2f89177de77d7771d594a9ee5
gavinfigueruelo <figueruelogavin@gmail.com>
2021-03-18 15:45:36 -0400

    correct functionality

--- frontend/static/src/components/Profile.js
+++ frontend/static/src/components/Profile.js
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Container,Row,Col,Form ,Button} from 'react-bootstrap';
-import Cookies from 'js-cookie';
-import {Redirect} from 'react-router-dom';
+import Cookies from "js-cookie";
+import { Redirect } from "react-router-dom";
 
-const axios = require('axios');
-
-
-class Profile extends Component{
+class Profile extends Component {
   constructor(props) {
-      super(props);
-
-  
+    super(props);
   }
 
   async componentDidMount() {
-      const response = await fetch("/rest-auth/user/");
-      const data = await response.json();
-      console.log(data);
-  }
-
-
+    const response = await fetch("/rest-auth/user/");
+    const data = await response.json();
+    console.log(data);
   }
 
   render() {
-      return (
-          <div className="">
-              <div className="card-header">
-                  <h1>Username</h1>
-                  <img src="#" alt="user"/>
-                  <p>My Bio:</p>
-              </div>
-              <div className="my_plants">
-                  <label htmlFor='myplants'>My Plants</label>
-                  <input type="text"/>
-              </div>
-            </div>
-
-      );
+    return (
+      <div className="container">
+        <div>
+          <h1>Username</h1>
+          <div classname="profpic">
+            <img src="#" alt="user" />
+          </div>
+          <div classname="bio">
+            <p>My Bio:</p>
+          </div>
+        </div>
+        <div className="my_plants">
+          <label htmlFor="myplants">My Plants</label>
+          <input type="text" />
+        </div>
+      </div>
+    );
   }
 }
 
